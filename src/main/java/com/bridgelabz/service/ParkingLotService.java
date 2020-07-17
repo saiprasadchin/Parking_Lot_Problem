a569cf6c582832a5ca644249f5297dc26697da4c
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-17 12:01:08 -0700

    Added : Test case to show capacity full

--- src/main/java/com/bridgelabz/service/ParkingLotService.java
+++ src/main/java/com/bridgelabz/service/ParkingLotService.java
@@ -2,9 +2,31 @@ package com.bridgelabz.service;
 
 import com.bridgelabz.model.Vehicle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotService {
 
+    public int parkingCapacity = 100;
+    public boolean isCapicityFull = false;
+    public List<Vehicle> parkedVehicles = new ArrayList<Vehicle>();
+
     public boolean parkVehicle(Vehicle vehicle) {
+        if(vehicle == null)
+            return false;
+        if(isCapicityFull){
+           return false;
+        }
+        if(parkedVehicles.contains(vehicle)){
+            return false;
+        }
+        parkedVehicles.add(vehicle);
+        if(parkedVehicles.size() == parkingCapacity)
+            isCapicityFull = true;
+        return true;
+    }
+
+    public boolean unParkVehicle(Vehicle vehicle) {
         if(vehicle == null)
             return false;
         return true;
--- src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
+++ src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
@@ -3,15 +3,36 @@ package com.bridgelabz.servicetest;
 import com.bridgelabz.model.Vehicle;
 import com.bridgelabz.service.ParkingLotService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotServiceTest {
 
+    ParkingLotService parkingLotService = null;
+    @Before
+    public void init(){
+        parkingLotService = new ParkingLotService();
+    }
+
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        Vehicle vehicle = new Vehicle();
-        ParkingLotService parkingLotService = new ParkingLotService();
+        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
         boolean isParked = parkingLotService.parkVehicle(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
+        boolean isUnParked = parkingLotService.unParkVehicle(vehicle);
+        Assert.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenAVehicles_WhenParkingLotIsFull_ShouldReturnFalse() {
+        parkingLotService.parkingCapacity = 1;
+        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
+        parkingLotService.parkVehicle(vehicle);
+        Assert.assertTrue(parkingLotService.isCapicityFull);
     }
 }
