600ec54d2195809cb0eb3b7217c24eec36ab8440
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-17 13:43:22 -0700

    Added : Test case for negative scenario

--- src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
+++ src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
@@ -10,14 +10,15 @@ import org.junit.Test;
 public class ParkingLotServiceTest {
 
     ParkingLotService parkingLotService = null;
+
     @Before
-    public void init(){
+    public void init() {
         parkingLotService = new ParkingLotService();
     }
-
+    //Test For Park The Vehicle
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
+        Vehicle vehicle = new Vehicle("Car", "Sai", "MH0404");
         boolean isParked = false;
         try {
             isParked = parkingLotService.parkVehicle(vehicle);
@@ -28,8 +29,20 @@ public class ParkingLotServiceTest {
     }
 
     @Test
+    public void givenANullVehicle_WhenParked_ShouldReturnTrue() {
+        Vehicle vehicle = null;
+        boolean isParked = false;
+        try {
+            isParked = parkingLotService.parkVehicle(vehicle);
+        } catch (ParkingLotServiceException e) {
+            Assert.assertEquals(ParkingLotServiceException.ExceptionType.INVALID_VEHICLE, e.exceptionType);
+        }
+    }
+
+    //Test For Check If Already vehicle is parked
+    @Test
     public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
-        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
+        Vehicle vehicle = new Vehicle("Car", "Sai", "MH0404");
         boolean isParked = false;
         try {
             isParked = parkingLotService.parkVehicle(vehicle);
@@ -40,11 +53,10 @@ public class ParkingLotServiceTest {
         Assert.assertFalse(isParked);
     }
 
-
+    //Test For UnPark The Vehicle
     @Test
     public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
-        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
-
+        Vehicle vehicle = new Vehicle("Car", "Sai", "MH0404");
         boolean isUnParked = false;
         try {
             parkingLotService.parkVehicle(vehicle);
@@ -55,9 +67,43 @@ public class ParkingLotServiceTest {
         Assert.assertTrue(isUnParked);
     }
 
+
+    @Test
+    public void givenNotParkedVehicle_WhenUnParked_ShouldThrowException() {
+        Vehicle vehicle = new Vehicle("Car", "Sai", "MH0404");
+        boolean isUnParked = false;
+        try {
+            isUnParked = parkingLotService.unParkVehicle(vehicle);
+        } catch (ParkingLotServiceException e) {
+            Assert.assertEquals(ParkingLotServiceException.ExceptionType.NO_SUCH_A_VEHICLE, e.exceptionType);
+        }
+    }
+    //Test To Handle Null Type Exception
+    @Test
+    public void givenANullVehicle_WhenUnParked_ShouldThrowException() {
+        Vehicle vehicle = null;
+        boolean isUnParked = false;
+        try {
+            isUnParked = parkingLotService.unParkVehicle(vehicle);
+        } catch (ParkingLotServiceException e) {
+            Assert.assertEquals(ParkingLotServiceException.ExceptionType.INVALID_VEHICLE, e.exceptionType);
+        }
+    }
+    //Test Case In Parking Lot Is Full
+    @Test
+    public void givenVehicles_WhenParkingLotIsFull_ShouldThrowException() {
+        parkingLotService.parkingCapacity = 1;
+        Vehicle vehicle = new Vehicle("Car", "Sai", "MH0404");
+        try {
+            parkingLotService.parkVehicle(vehicle);
+        } catch (ParkingLotServiceException e) {
+            Assert.assertEquals(ParkingLotServiceException.ExceptionType.PARKING_LOT_IS_FULL, e.exceptionType);
+        }
+    }
+
     @Test
     public void givenCapacity_WhenAvailableShould_InformToOWner() {
-        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
+        Vehicle vehicle = new Vehicle("Car", "Sai", "MH0404");
 
         boolean isUnParked = false;
         try {
@@ -67,16 +113,5 @@ public class ParkingLotServiceTest {
             e.printStackTrace();
         }
         Assert.assertFalse(parkingLotService.isCapicityFull);
-    }
-
-    @Test
-    public void givenAVehicles_WhenParkingLotIsFull_ShouldThrowException() {
-        parkingLotService.parkingCapacity = 1;
-        Vehicle vehicle = new Vehicle("Car","Sai","MH0404");
-        try {
-            parkingLotService.parkVehicle(vehicle);
-        } catch (ParkingLotServiceException e) {
-            Assert.assertEquals(ParkingLotServiceException.ExceptionType.PARKING_LOT_IS_FULL,e.exceptionType);
-        }
     }
 }
