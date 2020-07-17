6f0ae7cfd0145f1eea73cfccafdc97dfc9456e19
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-17 11:03:38 -0700

    Added : Test case for parking vehicle

--- src/main/java/com/bridgelabz/service/ParkingLotService.java
+++ src/main/java/com/bridgelabz/service/ParkingLotService.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.model.Vehicle;
+
 public class ParkingLotService {
+
+    public boolean parkVehicle(Vehicle vehicle) {
+        return true;
+    }
 }
--- src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
+++ src/test/java/com/bridgelabz/servicetest/ParkingLotServiceTest.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.servicetest;
 
+import com.bridgelabz.model.Vehicle;
+import com.bridgelabz.service.ParkingLotService;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotServiceTest {
+
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        Vehicle vehicle = new Vehicle();
+        ParkingLotService parkingLotService = new ParkingLotService();
+        boolean isParked = parkingLotService.parkVehicle(vehicle);
+        Assert.assertTrue(isParked);
+    }
 }
