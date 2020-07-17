cbb8af96ed90e7a50c477070824ad2a903df40e3
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-17 12:44:19 -0700

    Added : To inform security service when capacity is full

---
+++ src/main/java/com/bridgelabz/service/AirportSecurityService.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.service;
+
+public class AirportSecurityService {
+
+    private boolean isFull;
+
+    public boolean isFull() {
+        return isFull;
+    }
+
+    public void setFull(boolean full) {
+        isFull = full;
+    }
+}
--- src/main/java/com/bridgelabz/service/ParkingLotService.java
+++ src/main/java/com/bridgelabz/service/ParkingLotService.java
@@ -11,6 +11,8 @@ public class ParkingLotService {
     public int parkingCapacity = 100;
     public boolean isCapicityFull = false;
     public List<Vehicle> parkedVehicles = new ArrayList<Vehicle>();
+    public AirportSecurityService airportSecurityService = new AirportSecurityService();
+
 
     public boolean parkVehicle(Vehicle vehicle)throws ParkingLotServiceException {
         if(vehicle == null)
@@ -22,15 +24,16 @@ public class ParkingLotService {
             return false;
         }
         parkedVehicles.add(vehicle);
-        if(parkedVehicles.size() == parkingCapacity)
+        if(parkedVehicles.size() == parkingCapacity){
             isCapicityFull = true;
+            airportSecurityService.setFull(isCapicityFull);
+        }
         return true;
     }
 
     public boolean unParkVehicle(Vehicle vehicle)throws ParkingLotServiceException {
         if(vehicle == null)
             throw new ParkingLotServiceException(ParkingLotServiceException.ExceptionType.INVALID_VEHICLE, "Invalid Vehicle");
-
         return true;
     }
 }
